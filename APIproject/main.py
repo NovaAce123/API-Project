a316015704766eff7ed57526ddd05d84705bf096
NovaAce123 <98344753+NovaAce123@users.noreply.github.com>
2022-11-10 14:55:25 +0000

    asd

--- APIproject/main.py
+++ APIproject/main.py
@@ -7,7 +7,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-logs_file = Path(Path().resolve(), "log.txt")
+from APIproject.core.weather import weather
+
+logs_file = Path(Path().resolve(), "../log.txt")
 logs_file.touch(exist_ok=True)
 
 logging.basicConfig(
@@ -20,9 +22,10 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("../static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+weather_api = weather()
 
 @app.get("/")
 async def root():
@@ -40,6 +43,7 @@ async def read_item(item_id):
 async def get_weather(latitude: float = 51.5002, longitude: float = -0.120000124):
     log.info(f"Requested latitude: {latitude} and longitude: {longitude}")
     output = {}
+    weather_api.get_weather(longitude=longitude, latitude=latitude)
     return {"weather": output}
 
 
@@ -49,3 +53,5 @@ def html_output(request: Request):
         "index.html",
         {"request": request, "data": ["hello", 1, False]},
     )
+
+
